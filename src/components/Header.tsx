7ff56cc196897d073ad2ac6c5bc60e5caebad5ac
Hassaan Arain <rafatnaz21@gmail.com>
2024-12-08 04:54:48 +0500

    code pushed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,26 @@
+import { TfiMenuAlt } from "react-icons/tfi";
+
 import Image from "next/image";
 import SerachIcon from "../../public/images/Header/SearchIcon.png";
 import Like from "../../public/images/Header/Like.png";
 import CartIcon from "../../public/images/Header/Cart.png";
+import Nikee from "../../public/images/Header/Nike.png";
 export default function Header() {
   return (
-    <header className="h-[60px] bg-[#ffffff]">
+    <header className="h-[60px] bg-[#ffffff] my-3">
+      <section className="flex justify-center">
+        <div className="flex w-11/12  justify-between ">
+          <Image
+            src={Nikee}
+            alt="Nikee LOGO"
+            className="h-[20px] w-auto"
+          ></Image>
+          <div>
+            <TfiMenuAlt className="text-black" />
+          </div>
+        </div>
+      </section>
+
       <div className="h-[40px] flex items-center  justify-evenly">
         <div className="h-[30px] w-6/12 bg-[#F5F5F5] flex  items-center rounded-3xl ">
           <div className="w-3/6  flex justify-evenly">
