6cfe0630dd6045fb975be09069687eb81f5505a1
Hassaan Arain <rafatnaz21@gmail.com>
2024-12-08 03:56:43 +0500

    code pushed

--- src/components/Top-Header.tsx
+++ src/components/Top-Header.tsx
@@ -4,14 +4,18 @@ import Link from "next/link";
 export default function TopHeader() {
   return (
     <header className="h-[36px] bg-[#F5F5F5] flex items-center justify-around p-1">
-      <Image src={LOGO} alt="LOGO" className="h-[30px]"></Image>
-
-      <div>
-        <div className="h-[18px] text-[#111111] bg-[#ffffff] flex  items-center justify-center shadow-md">
-          <p className="h-[16] text-[10px]"> Skip to main content</p>
+      <div className="w-4/12 lg:w-3/12 ">
+        <Image src={LOGO} alt="LOGO" className="h-[30px]"></Image>
+      </div>
+      <div className="sm:flex   w-6/12 lg:w-8/12  h-[36px] sm:justify-evenly sm:items-center">
+        <div className="h-[18px] lg:h-[26px] w-36  text-[#111111] bg-[#ffffff] flex  items-center justify-center shadow-sm">
+          <p className="h-[16] text-[10px] lg:text-[12px] ">
+            {" "}
+            Skip to main content
+          </p>
         </div>
 
-        <nav className="text-[#111111] text-[12px]">
+        <nav className="text-[#111111] text-[10px]  lg:text-[15px]">
           <Link href={""} className="border-black border-r px-2">
             Find a Store
           </Link>
